803ebc0ab57cbe22cbf499b178139cb116141ccf
Krishna Chennapragada <krishna.ceee@gmail.com>
2019-01-24 19:13:19 -0800

    Created Project

---
+++ Finstagram/Finstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Finstagram
+//
+//  Created by Krishna Chenna on 1/24/19.
+//  Copyright © 2019 Krishna Chenna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
